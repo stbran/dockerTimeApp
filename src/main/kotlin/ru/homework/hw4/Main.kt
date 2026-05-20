0c4af1263169c76162f1cb67bf981860575aa6f6
YTamno <ytamno@inno.tech>
2026-05-20 22:30:15 +0300

    HW 4

---
+++ src/main/kotlin/ru/homework/hw4/Main.kt
@@ -0,0 +1,10 @@
+package ru.homework.hw4
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+fun main() {
+    val now = LocalDateTime.now()
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+    println("Текущее время: ${now.format(formatter)}")
+}
